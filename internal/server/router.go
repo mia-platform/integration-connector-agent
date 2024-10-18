bb0997b45cc81e56e29ab5cba4d9deea5fab6c94
Jacopo Andrea Giola <jacopo@giola.org>
2024-10-18 09:44:20 +0200

    refactor: dynamic service name based on executable name

--- internal/server/router.go
+++ internal/server/router.go
@@ -17,7 +17,9 @@ package server
 
 import (
 	"context"
+	"os"
 	"path"
+	"path/filepath"
 
 	"github.com/mia-platform/data-connector-agent/internal/config"
 	"github.com/mia-platform/data-connector-agent/internal/utils"
@@ -37,9 +39,10 @@ func NewRouter(env config.EnvironmentVariables, log *logrus.Logger) (*fiber.App,
 		DisableStartupMessage: true,
 	})
 
+	cmdName := filepath.Base(os.Args[0])
 	middlewareLog := glogrus.GetLogger(logrus.NewEntry(log))
 	app.Use(middleware.RequestMiddlewareLogger(middlewareLog, []string{"/-/"}))
-	statusRoutes(app, "data-connector-agent", utils.ServiceVersionInformation())
+	statusRoutes(app, cmdName, utils.ServiceVersionInformation())
 	if env.ServicePrefix != "" && env.ServicePrefix != "/" {
 		log.WithField("servicePrefix", env.ServicePrefix).Trace("applying service prefix")
 		app.Use(pprof.New(pprof.Config{Prefix: path.Clean(env.ServicePrefix)}))
@@ -49,7 +52,7 @@ func NewRouter(env config.EnvironmentVariables, log *logrus.Logger) (*fiber.App,
 		Context: context.Background(),
 		Openapi: &openapi3.T{
 			Info: &openapi3.Info{
-				Title:   "data-connector-agent",
+				Title:   cmdName,
 				Version: utils.Version,
 			},
 		},
