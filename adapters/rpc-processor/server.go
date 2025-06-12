6c9177943cf45698d63ad0d9cc8ac78bffbe0458
Federico Maggi <federico.maggi@mia-platform.eu>
2025-06-12 18:07:32 +0200

    refactor: improved interface to remove hc libs import from user

---
+++ adapters/rpc-processor/server.go
@@ -0,0 +1,50 @@
+// Copyright Mia srl
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rpcprocessor
+
+import (
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-plugin"
+	"github.com/mia-platform/integration-connector-agent/entities"
+)
+
+type Config struct {
+	Processors map[string]entities.InitializableProcessor
+	// TODO: wrap logger
+	Logger hclog.Logger
+}
+
+// handshakeConfigs are used to just do a basic handshake between
+// a plugin and host. If the handshake fails, a user friendly error is shown.
+// This prevents users from executing bad plugins or executing a plugin
+// directory. It is a UX feature, not a security feature.
+var handshakeConfig = plugin.HandshakeConfig{
+	ProtocolVersion:  1,
+	MagicCookieKey:   "integration-connector-agent-plugin",
+	MagicCookieValue: "go-plugin",
+}
+
+func Serve(config *Config) {
+	var pluginMap = map[string]plugin.Plugin{}
+	for name, processor := range config.Processors {
+		pluginMap[name] = &PluginAdapter{Impl: processor}
+	}
+
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: handshakeConfig,
+		Plugins:         pluginMap,
+	})
+}
